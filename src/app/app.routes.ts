0de9bfe17a53c54a110b147ab0bdbb46e24fa622
Kevin <eslick.kevin@yahoo.com>
2024-09-29 23:45:11 -0500

    setting up file structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent }, // Add this route
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // Optionally redirect root to dashboard
+];
---
+++ src/app/enviroment/enviroments.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  OAUTH_KEY:
+    'API KEY',
+};
